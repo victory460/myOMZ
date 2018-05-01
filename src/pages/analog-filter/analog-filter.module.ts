5aa66f756eae43e44d78ac937427db54ef9f9a23
victory460 <452962206@qq.com>
2018-05-01 19:40:20 +0800

    出现了PinoutPopover 等模块不能同时写在两个NgMoudle里面的问题

---
+++ src/pages/analog-filter/analog-filter.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../../app/shared/shared.module';
+import { IonicModule } from 'ionic-angular';
+
+import { AnalogFilterPage } from './analog-filter';
+import { SilverNeedleChart } from '../../components/chart/chart.component';
+import { XAxisComponent } from '../../components/xaxis-controls/xaxis-controls.component';
+import { YAxisComponent } from '../../components/yaxis-controls/yaxis-controls.component';
+import { TimelineChartComponent } from '../../components/timeline-chart/timeline-chart.component';
+import { TriggerComponent } from '../../components/trigger/trigger.component';
+import { FgenComponent } from '../../components/function-gen/function-gen.component';
+import { DigitalIoComponent } from '../../components/digital-io/digital-io.component';
+import { DcSupplyComponent } from '../../components/dc-supply/dc-supply.component';
+import { ModalCursorPage } from '../../pages/cursor-modal/cursor-modal';
+import { MathModalPage } from '../../pages/math-modal/math-modal';
+import { PinoutPopover } from '../../components/pinout-popover/pinout-popover.component';
+import { LaPopover } from '../../components/la-popover/la-popover.component';
+import { ChartAnnotationComponent } from '../../components/chart-annotation/chart-annotation.component';
+
+@NgModule({
+    imports: [
+        SharedModule,
+        IonicModule.forRoot(AnalogFilterPage),
+        IonicModule.forRoot(ModalCursorPage),
+        IonicModule.forRoot(MathModalPage),
+        IonicModule.forRoot(PinoutPopover),
+        IonicModule.forRoot(LaPopover),
+        IonicModule.forRoot(ChartAnnotationComponent)
+    ],
+    declarations: [
+        PinoutPopover,
+        LaPopover,
+        AnalogFilterPage,
+        ModalCursorPage,
+        MathModalPage,
+        SilverNeedleChart,
+        XAxisComponent,
+        YAxisComponent,
+        TimelineChartComponent,
+        TriggerComponent,
+        FgenComponent,
+        DigitalIoComponent,
+        DcSupplyComponent,
+        ChartAnnotationComponent
+    ],
+    exports: [AnalogFilterPage]
+})
+export class AnalogFilterModule { }
